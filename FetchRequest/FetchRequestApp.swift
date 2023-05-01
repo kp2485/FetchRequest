bdfa500908fffaf2a6b89d50a82cdccb5c18a630
Kyle Peterson <kdpeters@gmail.com>
2023-05-01 12:41:54 -0400

    Initial Commit

---
+++ FetchRequest/FetchRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRequestApp.swift
+//  FetchRequest
+//
+//  Created by Kyle Peterson on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
